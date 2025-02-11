137e62ea9bc54460b697015b84fb36ebd28b5ab8
SparqueeZ <baptiste.dijouxn8@gmail.com>
2025-02-11 14:10:39 +0100

    8e commit

--- src/main/java/org/javaee7/sample/MyApplication.java
+++ src/main/java/org/javaee7/sample/MyApplication.java
@@ -3,6 +3,8 @@ package org.javaee7.sample;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+// Salut
+
 @ApplicationPath("/resources")
 public class MyApplication extends Application {
 
